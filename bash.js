ee3a28d608ced732fcd9ce43428049bee07bfed7
Sonja Pylvainen <sonjapylvainen@gmail.com>
2021-08-06 15:06:28 -0400

    added pwd function

---
+++ bash.js
@@ -0,0 +1,18 @@
+process.stdout.write('prompt > ');
+
+// process.stdin.on('data', (data) => {
+//   const cmd = data.toString().trim();
+
+//   process.stdout.write("You typed: " + cmd);
+//   process.stdout.write('\nprompt >');
+// });
+
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  if(cmd === "pwd"){
+    console.log(process.cwd());
+  }
+
+});
